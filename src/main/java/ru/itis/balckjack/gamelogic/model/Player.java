ad1a24ed5417cdf64ee940b675bdbf257e35dfee
IMART0 <imartirosyan@vk.com>
2025-01-27 21:23:47 +0300

    Update Player.java

--- src/main/java/ru/itis/balckjack/gamelogic/model/Player.java
+++ src/main/java/ru/itis/balckjack/gamelogic/model/Player.java
@@ -6,8 +6,49 @@ import java.util.ArrayList;
 
 public class Player {
     private int id;
+
     private int bet;
     private int balance;
     private Socket socket;
     private ArrayList<Integer> hand;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getBet() {
+        return bet;
+    }
+
+    public void setBet(int bet) {
+        this.bet = bet;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void setSocket(Socket socket) {
+        this.socket = socket;
+    }
+
+    public ArrayList<Integer> getHand() {
+        return hand;
+    }
+
+    public void setHand(ArrayList<Integer> hand) {
+        this.hand = hand;
+    }
 }
